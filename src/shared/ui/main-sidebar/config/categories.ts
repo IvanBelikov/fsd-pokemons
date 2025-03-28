ba0879bb9273d49bd0f807cb73b0194a29a5cc35
Ivan Belikov <belikov.workmail@gmail.com>
2025-03-28 14:32:29 +0300

    review

--- src/shared/ui/main-sidebar/config/categories.ts
+++
@@ -1,7 +0,0 @@
-import { routes } from '@/shared/constants'
-
-export const categories = [
-    { label: 'Home', path: routes.HOME.path },
-    { label: 'Pokemons', path: routes.POKEMONS.path },
-    { label: 'Favorites', path: routes.FAVORITES.path },
-]
